27aa900495b0b5f325ab8362645fe3413f6389ed
DamienGrs <damien-31-14@outlook.fr>
2020-10-22 12:00:30 +0200

    Page admin

--- Vue/loginAdmin.php
+++ Vue/loginAdmin.php
@@ -9,38 +9,38 @@
         <header id="headerAdmin"> 
           <?php include("includes/nav.php"); ?>
           <section id="landing-section">
-            <div id="main-text-wrapper">
-                
-            </div>
+            <div id="main-text-wrapper"></div>
           </section>
         </header>
-        </section>
-        <div>
-            <div >
-                <h2>Log In</h2>
-            </div>
-            <div>
-                <label>Login</label>
-                <input id="login" value="">
-            </div>
-            <div>
-                <label>Mot de passe</label>
-                <input type="password" id="password" value="">
-            </div>
-            <div>
-                <button id="login" onclick="connectionAdmin()">Log In</button>
-            </div>
-            <div>
-                <p>
-                    Devenir membre ? <a href="createAdmin.php">Sign in</a>
-                </p>
-            </div>  
-        </div>
+        <main>
+            <section id="login-section">
+                <div class ="loginBox">
+                    <h2>Administrateur</h2>
+                    <div>
+                        <label>Nom d'utilisateur</label>
+                        <input id="login" value="">
+                    </div>
+                    <div>
+                        <label>Mot de passe</label>
+                        <input type="password" id="password" value="">
+                    </div>
+                    <div>
+                        <button id="login" onclick="connectionAdmin()" class="button-login">Log In</button>
+                    </div>
+                    <div>
+                        <p>
+                            Devenir membre ? <a href="createAdmin.php">Sign in</a>
+                        </p>
+                    </div>  
+                    </div>  
+            </section>
+        </main>
+        
     </body>
     <script type="text/javascript">
         function changeHeader(){
-            document.getElementById("headerAdmin").style.backgroundImage = "url('assets/admin.jpg')"; 
-            
+            document.getElementById("headerAdmin").style.backgroundImage = "url('assets/admin.jpg')";
+            document.getElementById("headerAdmin").style.opacity = "1";
         }
     </script>
 </html>
