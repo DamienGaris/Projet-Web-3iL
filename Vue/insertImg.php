c5808594df88b5e01d3ccde87baed941e3438428
DamienGrs <damien-31-14@outlook.fr>
2020-11-06 10:18:00 +0100

    Insert admin page

--- Vue/insertImg.php
+++ Vue/insertImg.php
@@ -1,23 +1,38 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Coucou</title>
-    <link href="style.css" rel="stylesheet" type="text/css">
+    <title>Insérer image</title>
+    <link href="style/style.css" rel="stylesheet" type="text/css">
     <script type="text/javascript" src="../Script/actionObject.js"></script>
     <script type="text/javascript" src="../Script/textAccueil.js"></script>
   </head>
-  <body>      
-  <div>
-    Création réussite de l'object !
-  </div>      
-        <form id="form2" action="../Controller/ctrlObject.php" method="post" enctype="multipart/form-data">
-            <div>
-                <label> Image:</label>
-                <input type="file" name="image"/>
-            </div>
-            <div>    
-                <button id="login" type="submit">Ajouter l'image à l'objet</button>
-            </div>
-        </form>
-    </body>
+  <body onload="changeHeader()">   
+    <header id="headerAdmin"> 
+      <?php include("includes/nav.php"); ?>
+    </header>   
+    <main class="login-main">
+            <section id="login-section">
+                <div class ="loginBox">
+                    <div class ="login-title">Création du produit réussie</div>
+                    <form id="form2" action="../Controller/ctrlObject.php" method="post" enctype="multipart/form-data">
+                      <div>
+                        <label>Image</label>
+                        <input type="file" name="image"/>
+                      </div>
+                      <div>    
+                        <button id="login" class="button-login" type="submit">Ajouter l'image du produit</button>
+                      </div>
+                    </form>
+            </section>
+        </main>    
+        
+  </body>
+  <script type="text/javascript">
+        function changeHeader(){
+            document.getElementById("headerAdmin").style.backgroundImage = "url()";
+            document.getElementById("headerAdmin").style.opacity = "1";
+            document.getElementById("headerAdmin").style.height = "77px";
+            document.getElementById("headerAdmin").style.borderBottom = "solid 2px";
+        }
+    </script>
 </html>
